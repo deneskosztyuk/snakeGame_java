0b9ba19b8735c7a313b3887a1109cd38f0ae5e64
Denes <denes.kosztyuk@gmail.com>
2022-12-09 15:28:28 +0000

    update snake game in java

---
+++ com/melonade/Snake.java
@@ -0,0 +1,31 @@
+package com.melonade;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Game());
+        setResizable(false);
+        pack();
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame ex = new Snake();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
+
